468a9615d21261d8b00188c2a473f4306ac9f68d
Batorov <dbatorov@gmail.com>
2019-06-14 21:47:09 +0300

    Алгоритмы. ДЗ 5. Collections Решена вторая задача

--- 01_Algorithms/HomeWork5/Alg_les5_task2.py
+++ 01_Algorithms/HomeWork5/Alg_les5_task2.py
@@ -3,17 +3,6 @@
 # Например, пользователь ввёл A2 и C4F. Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
 # Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 
-# 1 1                        A B 1
-# A B 1                      2 C F
-# 2 D F                -----------
-# -----      AB1 * F =     A 0 5 F
-# D 8 0      AB1 * C =     8 0 4 C
-#                      -----------
-#                        8 A 5 1 F
-#            AB1 * 2 = 1 5 6 2
-#                      -----------
-#                      1 E 0 7 1 F
-
 from collections import deque
 
 HEX = 16
@@ -21,32 +10,61 @@ LETTERS = '0123456789ABCDEF'
 MAPPING = dict([(LETTERS[i], i) for i in range(len(LETTERS))])
 
 
-def addition(a, b):
+# Функция сложения двух 16-еричных чисел. На вход строки или deque. Вывод в deque
+def hex_addition(a_str, b_str):
+    a = deque(a_str)
+    b = deque(b_str)
     res = deque()  #результат сложения
     rem = 0  #остаток
     while a or b:
         a_num = MAPPING[a.pop()] if a else 0
         b_num = MAPPING[b.pop()] if b else 0
-        res.appendleft(LETTERS[(a_num + b_num + rem) % HEX])
-        rem = (a_num + b_num + rem) // HEX
+
+        temp = a_num + b_num + rem
+        res.appendleft(LETTERS[temp % HEX])
+        rem = temp // HEX
     if rem:
         res.appendleft(LETTERS[rem])
     return res
 
 
-def multiplication(a, b):
-    res = deque()  #результат умножения
+# Функция умножения 16-еричного числа на 16-чную "цифру"
+def hex_digit_mul(a_str, digit):
+    a = deque(a_str)
+    res = deque()  #результат сложения
+    rem = 0  #остаток
+    while a:
+        a_num = MAPPING[a.pop()]
+
+        temp = a_num * digit + rem
+        res.appendleft(LETTERS[temp % HEX])
+        rem = temp // HEX
+    if rem:
+        res.appendleft(LETTERS[rem])
+    return res
+
+
+# Функция умножения двух 16-еричных чисел. На вход строки или deque. Вывод в deque
+def hex_multiplication(a_str, b_str):
+    a = deque(a_str)
+    res = '0'  #результат умножения
+    for i, item in enumerate(reversed(a)):
+        res = hex_addition(hex_digit_mul(b_str, MAPPING[item]) + deque('0' * i), res)
     return res
 
 
 if __name__ == '__main__':
-    operator = input('Введите оператор. + или *. Только один символ: ')
-    x = deque(input('Введите первое число в 16-тиричной системе: ').upper())
-    y = deque(input('Введите второе число в 16-тиричной системе: ').upper())
+    operator = None
+    while operator not in ['*', '+']:
+        operator = input('Введите оператор. + или *. Только один символ: ')
+
+    x = input('Введите первое число в 16-тиричной системе: ').upper()
+    y = input('Введите второе число в 16-тиричной системе: ').upper()
 
     if operator == '+':
-        r = addition(x, y)
-    elif operator == '*':
-        r = multiplication(x, y)
+        r = hex_addition(x, y)
+    else:
+        r = hex_multiplication(x, y)
 
-    print('Ответ: ', *r, sep='')
+    print(f'{x} {operator} {y} = ', end='')
+    print(*r, sep='')
